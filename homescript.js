201f4727a8b424b819d67eba854ae3e97654c841
Saman Givian <samgivian2015@gmail.com>
2021-03-15 10:22:21 -0700

    Add files via upload

--- homescript.js
+++ homescript.js
@@ -1,39 +1,40 @@
-$(function(){
-	$(".typewriter").typed({
-		strings: ["Student at ... ", " Software Engineer.", "Nice to meet you."],
-		// optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-		stringsElement: null,
-		// typing speed
-		typeSpeed: 30,
-		// time before typing starts
-		startDelay: 1500,
-		// backspacing speed
-		backSpeed: 20,
-		// time before backspacing
-		backDelay: 1000,
-		// loop
-		loop: true,
-		// false = infinite
-		loopCount: false,
-		// show cursor
-		showCursor: false,
-		// character for cursor
-		cursorChar: "|",
-		// attribute to type (null == text)
-		attr: null,
-		// either html or text
-		contentType: 'html',
-		// call when done callback function
-		callback: function() {},
-		// starting callback function before each string
-		preStringTyped: function() {},
-		//callback for every typed string
-		onStringTyped: function() {},
-		// callback for reset
-		resetCallback: function() {}
-	});
+$(function () {
+    $(".typewriter").typed({
+        strings: ["Student at ... ", " Software Engineer.", "Nice to meet you."],
+        // optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 30,
+        // time before typing starts
+        startDelay: 1500,
+        // backspacing speed
+        backSpeed: 20,
+        // time before backspacing
+        backDelay: 1000,
+        // loop
+        loop: true,
+        // false = infinite
+        loopCount: false,
+        // show cursor
+        showCursor: false,
+        // character for cursor
+        cursorChar: "|",
+        // attribute to type (null == text)
+        attr: null,
+        // either html or text
+        contentType: 'html',
+        // call when done callback function
+        callback: function () { },
+        // starting callback function before each string
+        preStringTyped: function () { },
+        //callback for every typed string
+        onStringTyped: function () { },
+        // callback for reset
+        resetCallback: function () { }
+    });
     $(".icon-bar").fadeIn(1500);
     $(".introduction").fadeIn(1500);
+
 });
 
 function toggleMenu() {
@@ -47,6 +48,8 @@ function toggleMenu() {
 var navHeight = 90;
 var scrollNavHeight = 65;
 var isExpanded = true;
+
+var window_height = window.screen.height;
 
 let last_known_scroll_position = 0;
 let ticking = false;
@@ -85,24 +88,34 @@ window.addEventListener('scroll', function (e) {
 
             if ($(window).scrollTop() > navHeight) {
                 $('.aa-nav').addClass('aa-small-nav');
-                $('.aa-nav-icon').addClass('aa-small-nav-icon');
                 $('.aa-nav-items').addClass('aa-small-nav-items');
                 $('.aa-hamburger-menu').addClass('aa-small-hamburger-menu');
                 $('.aa-nav-item > a').css('color', 'black');
                 $('.aa-nav ').css('background-color', '#3e78b2');
                 isExpanded = false;
             }
-
-            if (!isExpanded && $(window).scrollTop() < navHeight) {
-                $('.aa-nav').removeClass('aa-small-nav');
-                $('.aa-nav-icon').removeClass('aa-small-nav-icon');
-                $('.aa-nav-items').removeClass('aa-small-nav-items');
-                $('.aa-hamburger-menu').removeClass('aa-small-hamburger-menu');
-                $('.aa-nav-item > a').css('color', 'black');
-                $('.aa-nav ').css('background-color', 'transparent ');
-                isExpanded = true;
+            if ($(window).scrollTop() < navHeight) {
+                $('.aa-nav ').css('background-color', 'transparent')
+                $('.aa-nav ').removeClass('aa-small-nav')
             }
+            if ($(window).scrollTop() > navHeight)
+                if (!isExpanded && $(window).scrollTop() < navHeight) {
+                    $('.aa-nav').removeClass('aa-small-nav');
+                    $('.aa-nav-items').removeClass('aa-small-nav-items');
+                    $('.aa-hamburger-menu').removeClass('aa-small-hamburger-menu');
+                    $('.aa-nav-item > a').css('color', 'black');
+                    $('.aa-nav ').css('background-color', 'transparent ');
+                    isExpanded = true;
+                }
             ticking = false;
+
+            if ($(window).scrollTop() > window_height / 10) {
+                var d = document.getElementById("id-h-about");
+                d.className += " h-about-animation";
+                d = document.getElementById("id-h-about-p");
+                d.className += " h-about-animation-p";
+                
+            }
         });
 
         ticking = true;
